cccf58974819c54a2b13c9f7e6469a87c48cfdc0
Sergey Oleynik <s-oleynik@marya.ru>
2016-04-28 10:28:20 +0300

    #458 убрана рамка и уменьшен шрифт label для дизайна эльдарадо

--- scripts/main.js
+++ scripts/main.js
@@ -5,7 +5,45 @@ $(document).ready(function() {
         Models: {},
         Collections: {},
         Views: {},
-        Router: {}
+        Router: {},
+        Helpers: {}
+    };
+    /**
+     *  Ajax подгрузка шаблона
+     *  */
+    App.Helpers.TemplateManager = {
+        templates: {},
+
+        get: function (id, callback) {
+            var template = this.templates[id];
+
+            if (template) {
+                callback(template);
+
+            } else {
+
+                var that = this;
+                $.ajaxPrefilter(function (options, originalOptions, jqXHR) {
+                    options.crossDomain = {
+                        crossDomain: true
+                    };
+                    options.xhrFields = {
+                        withCredentials: true
+                    };
+                });
+                $.ajax({
+                    url: "http://module.infcentre.ru/module/" + id,
+                    success: function (template) {
+                        var tmpl = template;
+                        that.templates[id] = tmpl;
+                        callback(tmpl);
+                    }
+                });
+
+            }
+
+        }
+
     };
 
     function init(option) {
