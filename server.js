aa624b542234a577115c23d3f7b6b806a3651068
Sergey Oleynik <s-oleynik@marya.ru>
2016-05-05 12:00:18 +0300

    Правки для тестового сервера

--- server.js
+++ server.js
@@ -2,23 +2,30 @@ module.exports = function(app){
     var https        = require('https'),
         http         = require('http'),
         fs           = require('fs');
-    var options = {
-        key: fs.readFileSync('../ssl/certificate.key'),
-        cert: fs.readFileSync('../ssl/certificate.crt')
-    };
 
     app.set('port', process.env.PORT || 80);
-    app.set('portSSL', process.env.PORTSSL || 443);
 
     var httpServer = http.createServer(app);
-    var httpsServer = https.createServer(options, app);
 
     httpServer.listen(app.get('port'), function () {
         console.log(new Date().toISOString() + ': server started on port ' + app.get('port') +'!');
     });
-    httpsServer.listen(app.get('portSSL'), function () {
-        console.log(new Date().toISOString() + ': server started on port ' + app.get('portSSL') +'!');
-    });
+
+    var options;
+
+    try {
+        options = {
+            key: fs.readFileSync('../ssl/certificate.key'),
+            cert: fs.readFileSync('../ssl/certificate.crt')
+        };
+        app.set('portSSL', process.env.PORTSSL || 443);
+        var httpsServer = https.createServer(options, app);
+        httpsServer.listen(app.get('portSSL'), function () {
+            console.log(new Date().toISOString() + ': serverSSL started on port ' + app.get('portSSL') +'!');
+        });
+    } catch (e) {
+        console.error(new Date().toISOString() + ': serverSSL not started!');
+    }
 
     return app;
 };
