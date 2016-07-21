719ee0efcb80c863846c9381a7c5bdb3afdce6dc
Sergey Oleynik <s-oleynik@marya.ru>
2016-07-21 11:22:16 +0300

    #950 Запретить инициализацию модуля, если сайт указан не верно. Логирование ошибок запуска

--- scripts/models/config.js
+++ scripts/models/config.js
@@ -28,20 +28,34 @@ module.exports = Backbone.Ribs.Model.extend({
     },
 
     checkConfig: function () {
-        if (!this.get('serverUrl'))
-            throw new Error("Не указано имя сервера Мария serverUrl:'" + this.get('serverUrl') + "' проверьте конфигурацию");
-        if (!this.get('siteUrl'))
-            throw new Error("Не указано имя вашего сайта siteUrl:'" + this.get('siteUrl') + "' проверьте конфигурацию");
-        if (!this.get('initType'))
-            throw new Error("Не указан тип модуля initType:'" + this.get('initType') + "' проверьте конфигурацию");
-        if (!this.get('initPosition'))
-            throw new Error("Не указан способ инициализации initPosition:'" + this.get('initPosition') + "' проверьте конфигурацию");
-        if (this.get('siteUrl') != window.location.hostname && this.get('siteUrl') != window.location.href)
-            console.warn('Ваш сайт URL "' + window.location.hostname + '" не соответствует указанному в конфигураторе "' + this.get('siteUrl') +'"');
-        if (!this.get('style'))
-            console.warn('Стили отключены');
-        if (!this.get('showMap'))
-            console.warn('Карта отключена');
+        try {
+            if (!this.get('serverUrl'))
+                throw new Error("Не указано имя сервера Мария serverUrl:'" + this.get('serverUrl') + "' проверьте конфигурацию");
+            if (!this.get('siteUrl'))
+                throw new Error("Не указано имя вашего сайта siteUrl:'" + this.get('siteUrl') + "' проверьте конфигурацию");
+            if (!this.get('initType'))
+                throw new Error("Не указан тип модуля initType:'" + this.get('initType') + "' проверьте конфигурацию");
+            if (!this.get('initPosition'))
+                throw new Error("Не указан способ инициализации initPosition:'" + this.get('initPosition') + "' проверьте конфигурацию");
+            if (this.get('siteUrl') != window.location.hostname && this.get('siteUrl') != window.location.href) {
+                if (UM.conf.server.type == 'prod') {
+                    throw new Error('Ваш сайт URL "' + window.location.hostname + '" не соответствует указанному в конфигураторе "' + this.get('siteUrl') +'"');
+                } else {
+                    console.warn('Ваш сайт URL "' + window.location.hostname + '" не соответствует указанному в конфигураторе "' + this.get('siteUrl') +'"');
+                }
+            }
+            if (UM.conf.server.type != 'prod') {
+                if (!this.get('style'))
+                    console.warn('Стили отключены');
+                if (!this.get('showMap'))
+                    console.warn('Карта отключена');
+            }
+
+        } catch (err) {
+            new UM.Models.Logger({message: String(err)});
+            throw new Error(err);
+        }
+
     },
 
     initForm: function () {
@@ -56,7 +70,9 @@ module.exports = Backbone.Ribs.Model.extend({
         if (this.get('formType') == 'calculation' || this.get('formType') == 'measurement' || this.get('formType') == 'credit') {
             this.form = new UM.Models.User(this.data.user, this.get('formConfig'));
         } else {
-            throw new Error("Тип заявки '" + this.get('formType') + "' не поддерживается или не корректен");
+            var msgErr = "Тип заявки '" + this.get('formType') + "' не поддерживается или не корректен";
+            new UM.Models.Logger({message: String(msgErr)});
+            throw new Error(msgErr);
         }
     }
 });
