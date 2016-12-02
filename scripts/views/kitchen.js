cf51a647fbd33e2f6b433d89cb2fe26e03105a8f
Julia Starzhinskaya <yu-starzhinskaya@marya.ru>
2016-12-02 09:24:04 +0300

    fix kitchens

--- scripts/views/kitchen.js
+++ scripts/views/kitchen.js
@@ -1,3 +1,3 @@
 module.exports = UM.Views.InputSelectOption.extend({
-    template: _.template('<img src="<%= preview %>" alt="" class="um-dropdown-img"><span><%= name %></span>')
+    template: _.template('<%if(preview != ""){ %><img src="<%= preview %>" alt="" class="um-dropdown-img"><%}%><span><%= name %></span>')
 });
